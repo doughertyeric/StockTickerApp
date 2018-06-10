cad557954e404ea34ac8b10360e7e99447eea815
doughertyeric <dougherty.eric@gmail.com>
2018-06-10 16:57:11 -0400

    Changed access to heroku site

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ def index():
  
 if __name__ == "__main__":
     print("STARTED")
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    #app.run(debug=True)
